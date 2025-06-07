8100788b1f77d402464f0a86b7a4fa50625296eb
ankitojha07 <ankitojha1409@gmail.com>
2025-06-07 13:27:26 +0530

    impklemented navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white px-6 py-4 flex items-center justify-between">
+      <div className="font-bold text-xl">
+        <Link href="/">EventHub</Link>
+      </div>
+      <div className="space-x-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/events" className="hover:underline">
+          Events
+        </Link>
+        <Link href="/admin" className="hover:underline">
+          Admin
+        </Link>
+      </div>
+    </nav>
+  );
+}
